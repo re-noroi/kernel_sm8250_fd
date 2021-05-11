b40b2453eb0a949cee8b21c21406f61334f06874
Gautham R. Shenoy <ego@linux.vnet.ibm.com>
2021-05-11 20:46:09 +0530

    sched/fair: Fix clearing of has_idle_cores flag in select_idle_cpu()
    In commit:
    
      9fe1f127b913 ("sched/fair: Merge select_idle_core/cpu()")
    
    in select_idle_cpu(), we check if an idle core is present in the LLC
    of the target CPU via the flag "has_idle_cores". We look for the idle
    core in select_idle_cores(). If select_idle_cores() isn't able to find
    an idle core/CPU, we need to unset the has_idle_cores flag in the LLC
    of the target to prevent other CPUs from going down this route.
    
    However, the current code is unsetting it in the LLC of the current
    CPU instead of the target CPU. This patch fixes this issue.
    
    Fixes: 9fe1f127b913 ("sched/fair: Merge select_idle_core/cpu()")
    Signed-off-by: Gautham R. Shenoy <ego@linux.vnet.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Acked-by: Mel Gorman <mgorman@techsingularity.net>
    Link: https://lore.kernel.org/r/1620746169-13996-1-git-send-email-ego@linux.vnet.ibm.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7160,7 +7160,7 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, bool 
 	}
 
 	if (has_idle_core)
-		set_idle_cores(this, false);
+		set_idle_cores(target, false);
 
 	if (sched_feat(SIS_PROP) && this_sd && !has_idle_core) {
 		time = cpu_clock(this) - time;
