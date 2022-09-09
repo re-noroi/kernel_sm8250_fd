862ce49564c7c5d9df823e57ac13f3bc7606fd15
Nikhil V <quic_nprakash@quicinc.com>
2022-09-09 14:04:17 +0530

    ANDROID: mm: cma: proper ret type for tasks interrupted by fatal signal
    Currently, cma_alloc() checks if the task has fatal signals pending
    and simply bail out without updating return type(defaulting to EBUSY).
    Update the return type to EINTR when cma allocation fails due to pending
    fatal signals.
    
    Bug: 245880180
    Change-Id: Ibcef026a3af3417a5a10e0eae51017f477f6ac0f
    Signed-off-by: Nikhil V <quic_nprakash@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/cma.c
+++ mm/cma.c
@@ -491,8 +491,10 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 			if ((num_attempts < max_retries) && (ret == -EBUSY)) {
 				mutex_unlock(&cma->lock);
 
-				if (fatal_signal_pending(current))
+				if (fatal_signal_pending(current)) {
+					ret = -EINTR;
 					break;
+				}
 
 				/*
 				 * Page may be momentarily pinned by some other
