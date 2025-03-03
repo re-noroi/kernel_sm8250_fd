fee4bf60e4ee87252fb5ad8f3a42e004d083104e
Xuewen Yan <xuewen.yan@unisoc.com>
2025-03-03 18:52:39 +0800

    sched/fair: Fixup-wake_up_sync-vs-DELAYED_DEQUEUE
    Delayed dequeued feature keeps a sleeping task enqueued until its
    lag has elapsed. As a result, it stays also visible in rq->nr_running.
    So when in wake_affine_idle(), we should use the real running-tasks
    in rq to check whether we should place the wake-up task to
    current cpu.
    On the other hand, add a helper function to return the nr-delayed.
    
    Fixes: 152e11f6df29 ("sched/fair: Implement delayed dequeue")
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Tested-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/all/20250303105241.17251-2-xuewen.yan@unisoc.com/
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6386,6 +6386,11 @@ static bool dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 #ifdef CONFIG_SMP
 
+static inline unsigned int cfs_h_nr_delayed(struct rq *rq)
+{
+	return (rq->cfs.h_nr_running - rq->cfs.h_nr_runnable);
+}
+
 /* Working cpumask for: load_balance, load_balance_newidle. */
 DEFINE_PER_CPU(cpumask_var_t, load_balance_mask);
 DEFINE_PER_CPU(cpumask_var_t, select_idle_mask);
@@ -6539,9 +6544,12 @@ wake_affine_idle(int this_cpu, int prev_cpu, int sync)
 	if (available_idle_cpu(this_cpu) && cpus_share_cache(this_cpu, prev_cpu))
 		return available_idle_cpu(prev_cpu) ? prev_cpu : this_cpu;
 
-	if (sync && cpu_rq(this_cpu)->nr_running == 1)
-		return this_cpu;
+	if (sync) {
+		struct rq *rq = cpu_rq(this_cpu);
 
+		if ((rq->nr_running - cfs_h_nr_delayed(rq)) == 1)
+			return this_cpu;
+	}
 	if (available_idle_cpu(prev_cpu))
 		return prev_cpu;
 
