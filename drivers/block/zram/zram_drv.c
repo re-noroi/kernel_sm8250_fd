a46407dfc5c0fcf0d4536bce1ddab8a13dcd8a6f
Sergey Senozhatsky <senozhatsky@chromium.org>
2025-06-27 16:18:21 +0900

    zram: pass buffer offset to zcomp_available_show()
    In most cases zcomp_available_show() is the only emitting
    function that is called from sysfs read() handler, so it
    assumes that there is a whole PAGE_SIZE buffer to work with.
    There is an exception, however: recomp_algorithm_show().
    
    In recomp_algorithm_show() we prepend the buffer with
    priority number before we pass it to zcomp_available_show(),
    so it cannot assume PAGE_SIZE anymore and must take
    recomp_algorithm_show() modifications into consideration.
    Therefore we need to pass buffer offset to zcomp_available_show().
    
    Also convert it to use sysfs_emit_at(), to stay aligned
    with the rest of zram's sysfs read() handlers.
    
    On practice we are never even close to using the whole PAGE_SIZE
    buffer, so that's not a critical bug, but still.
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Link: https://lore.kernel.org/all/20250627071840.1394242-1-senozhatsky@chromium.org/#Z31drivers:block:zram:zcomp.h
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1160,12 +1160,13 @@ static void comp_algorithm_set(struct zram *zram, u32 prio, const char *alg)
 	zram->comp_algs[prio] = alg;
 }
 
-static ssize_t __comp_algorithm_show(struct zram *zram, u32 prio, char *buf)
+static ssize_t __comp_algorithm_show(struct zram *zram, u32 prio,
+				     char *buf, ssize_t at)
 {
 	ssize_t sz;
 
 	down_read(&zram->init_lock);
-	sz = zcomp_available_show(zram->comp_algs[prio], buf);
+	sz = zcomp_available_show(zram->comp_algs[prio], buf, at);
 	up_read(&zram->init_lock);
 
 	return sz;
@@ -1312,7 +1313,7 @@ static ssize_t comp_algorithm_show(struct device *dev,
 {
 	struct zram *zram = dev_to_zram(dev);
 
-	return __comp_algorithm_show(zram, ZRAM_PRIMARY_COMP, buf);
+	return __comp_algorithm_show(zram, ZRAM_PRIMARY_COMP, buf, 0);
 }
 
 static ssize_t comp_algorithm_store(struct device *dev,
@@ -1341,7 +1342,7 @@ static ssize_t recomp_algorithm_show(struct device *dev,
 			continue;
 
 		sz += scnprintf(buf + sz, PAGE_SIZE - sz - 2, "#%d: ", prio);
-		sz += __comp_algorithm_show(zram, prio, buf + sz);
+		sz += __comp_algorithm_show(zram, prio, buf, sz);
 	}
 
 	return sz;
